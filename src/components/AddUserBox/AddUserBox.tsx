de6bdf32a79b381885adf1e5ba326dcaecec1a77
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2020-08-28 10:27:23 +0900

    Add: Add user modal style
    회원 추가 모달 스타일 추가

--- src/components/AddUserBox/AddUserBox.tsx
+++ src/components/AddUserBox/AddUserBox.tsx
@@ -4,9 +4,11 @@ import { IoMdPersonAdd } from 'react-icons/io'
 import Modal from 'react-modal';
 
 import './AddUserBox.scss';
+import { stores } from '../../stores';
 
 const AddUserBox = () => {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  const [modalIsOpen, setIsOpen] = React.useState(true);
 
   const openModal = () => {
     setIsOpen(true);
@@ -16,28 +18,78 @@ const AddUserBox = () => {
     setIsOpen(false);
   }
 
+  const customStyles: ReactModal.Styles = {
+    overlay: {
+      backgroundColor: 'rgba(0,0,0,0.4)'
+    },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      border: 'none',
+      borderRadius: '3px',
+      padding: '60px 80px'
+    },
+  }
+
+  const applyUser = async () => {
+  }
+
   return (
     <div className='AddUserBox'>
-      <IoMdPersonAdd color='a0a0a0' />
+      <IoMdPersonAdd className='IoMPersonAdd' color='a0a0a0' onClick={openModal} />
+
       <Modal
         isOpen={modalIsOpen}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)'
-          }
-        }}
+        onRequestClose={closeModal}
+        style={customStyles}
       >
-        <h3>회원 추가</h3>
-        <p>본인의 Github 아이디를 입력해주세요</p>
+        <h2 style={{
+          fontSize: '16px',
+          marginBottom: '30px',
+          color: '#ff6f61',
+        }}
+        >본인의 Github 아이디를 입력해주세요</h2>
 
-        <input type="text" placeholder='Github 아이디' />
-        <button>확인</button>
-        <button>취소</button>
+        <div style={{
+          display: 'felx',
+        }}>
+          <input type="text" placeholder='Github 아이디' style={{
+            boxSizing: 'border-box',
+            border: 'none',
+            padding: '10px',
+            width: '100%',
+            marginBottom: '10px',
+          }} />
+          <input type="text" placeholder='이름' style={{
+            boxSizing: 'border-box',
+            border: 'none',
+            padding: '10px',
+            width: '100%',
+            marginBottom: '10px',
+          }} />
+        </div>
+        <p style={{
+          fontSize: '12px',
+          color: '#808080',
+          margin: 0,
+        }}>이름을 입력하지 않을 시 Github 이름이 적용됩니다</p>
+        <div style={{
+          textAlign: 'center',
+          marginTop: '50px',
+        }}>
+          <input type="button" value="신청" style={{
+            padding: '5px 10px',
+            width: '60px',
+            border: '1px solid #ff6f61',
+            borderRadius: '3px',
+            backgroundColor: '#ff6f61',
+            color: '#ffffff',
+          }} onClick={applyUser} />
+        </div>
       </Modal>
     </div >
   )
